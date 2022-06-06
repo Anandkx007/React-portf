795420bdf4afda341815ad10a4bae11795484866
unknown <Anandkx007@gmail.com>
2022-06-06 12:42:34 +0530

    Updates

---
+++ precache-manifest.3ef8ec88c146627266cc8dc7b21c6573.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52d0b399413194d2b30f8eb67bb7ce6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "44cbd0ceb2007ddf9401",
+    "url": "/portfolio/static/css/main.5cbafb93.chunk.css"
+  },
+  {
+    "revision": "f125a787542f747a7369",
+    "url": "/portfolio/static/js/2.03d511f6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.03d511f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44cbd0ceb2007ddf9401",
+    "url": "/portfolio/static/js/main.55c6b966.chunk.js"
+  },
+  {
+    "revision": "309a9e55c61ba5bff665",
+    "url": "/portfolio/static/js/runtime-main.b3df5f95.js"
+  }
+]);
